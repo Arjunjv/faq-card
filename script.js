f18261ab6fc3b86d82114a73fb761edb40555143
Arjun J V <87376505+Arjunjv@users.noreply.github.com>
2022-04-19 18:25:13 +0530

    uploaded mandatory files

---
+++ script.js
@@ -0,0 +1,25 @@
+$(".ans").hide();
+$("#ques1,#down1").click(function(){
+	$("#ans1").slideToggle();
+	$("#ques1").css("font-weight","700");
+});
+$("#ques2,#down2").click(function(){
+	$("#ans2").slideToggle();
+	$("#ques2").css("font-weight","700");
+	
+});
+$("#ques3,#down3").click(function(){
+	$("#ans3").slideToggle();
+	$("#ques3").css("font-weight","700");
+	
+});
+$("#ques4,#down4").click(function(){
+	$("#ans4").slideToggle();
+	$("#ques4").css("font-weight","700");
+	$("down4").css("transform","rotate(180deg)");
+});
+$("#ques5,#down5").click(function(){
+	$("#ans5").slideToggle();
+	$("#ques5").css("font-weight","700");
+
+});
